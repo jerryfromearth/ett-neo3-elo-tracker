5760fa6979a0540a8f2266dee1370f86a997b9e2
jerryfromearth <jerryfromearth@gmail.com>
2021-09-09 12:43:16 +0200

    improved output

---
+++ README.md
@@ -0,0 +1,9 @@
+Logs >=2000 ELO ETT players in intervals.
+
+Usage:
+
+```
+npm install
+npm start
+```
+Then check out db.json once in a while. All Neo3 players that had >=2000 ELO will be saved in there.
--- app.js
+++ app.js
@@ -1,41 +1,65 @@
 const fetch = require("node-fetch");
-const fs = require("fs");
+const nconf = require("nconf");
 
-function parseJson(json, fp) {
+let dbFile = "db.json";
+function parseJson(json) {
   pico_users = json.OnlineUses.filter((obj) => obj.Device === "Pico Neo 3");
   pico_users = pico_users.map((user) => {
     user.ELO = Number(user.ELO);
     user.Id = Number(user.Id);
+    user.Time = new Date().toISOString();
     return user;
   });
-  pico_users = pico_users.sort((user1, user2) => {
-    return user2.ELO - user1.ELO;
-  });
 
-  let highest = pico_users[0];
-  highest.Time = new Date().toISOString();
-  console.log(pico_users[0]);
+  pico_users = pico_users.filter((user) => user.ELO >= 2000);
 
-  fs.appendFileSync("log.txt", JSON.stringify(pico_users[0], null, 2) + "\n");
+  console.log(new Date().toISOString(), pico_users);
+  return pico_users;
 }
 
-function loadAndParse() {
+async function loadAndParse() {
+  // Parse users from json
+  nconf.file(dbFile);
+  let users = nconf.get("users") ?? [];
+  console.log("Users:", JSON.stringify(users));
+
+  // Fetch new_users
+  let new_users = [];
   try {
     let url =
       "http://elevenlogcollector-env.js6z6tixhb.us-west-2.elasticbeanstalk.com/ElevenServerLiteSnapshot";
     let settings = { method: "Get" };
-    fetch(url, settings)
-      .then((res) => res.json())
-      .then((json) => {
-        parseJson(json);
-      });
+    let res = await fetch(url, settings);
+    let json = await res.json();
+    new_users = parseJson(json);
   } catch (error) {
     console.error(error);
+    return;
   }
+  console.log("New Users:", JSON.stringify(new_users));
+
+  // Merge new_users into users
+  new_users.forEach((new_user) => {
+    let existing_users = users.filter((user) => user.Id === new_user.Id);
+    if (existing_users.length === 0) {
+      users.push(new_user);
+    } else {
+      let existing_user = existing_users[0];
+      if (new_user.ELO >= existing_user.ELO) {
+        Object.assign(existing_user, new_user);
+      }
+    }
+  });
+
+  users = users.sort((user1, user2) => {
+    return user2.ELO - user1.ELO;
+  });
+  console.log("Merged&sorted users:", JSON.stringify(users));
+
+  nconf.set("users", users);
+  nconf.save();
 }
+
 loadAndParse();
-let minutes = 10;
+let minutes = 1;
 setInterval(loadAndParse, 1000 * 60 * minutes);
-// const fs=require('fs');
-// let json=JSON.parse(fs.readFileSync('example.json'));
-// parseJson(json);
